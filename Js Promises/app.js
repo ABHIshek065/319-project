60bcaada4eb79064cf6e11891f551d0391f33bcb
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2023-03-29 23:54:56 +0530

    up

--- Js Promises/app.js
+++ Js Promises/app.js
@@ -25,15 +25,21 @@
 // })
 
 
-function GenrateNew(){
-    
+function GenerateNew(){
+
+
+    fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+    //   console.log(data);
+
+      let random = Math.floor(Math.random()*data.length)
+      document.getElementById('quotes').innerText = data[random].text;
+      document.getElementById('author').innerText = data[random].author;
+    });
 }
 
 
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+
