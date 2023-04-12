b439fc9ea810be463f9e31b648818dcc184f807d
Abhishek Patel <98528020+Abhi8755@users.noreply.github.com>
2023-04-12 10:09:10 +0530

    formate date

---
+++ testing todos/app.js
@@ -0,0 +1,42 @@
+let btn = document.getElementById("btn");
+
+const users = [];
+const options = {weekday: 'long', day: 'numeric', month:'long'}
+
+renderNames = () => {
+  let nameContainer = document.getElementById("nameContainer");
+  nameContainer.innerHTML = "";
+  users.map((user) => {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    let p = document.createElement("p");
+
+    let input = document.createElement("p");
+
+    p.innerText = user.name;
+    let dateToFormat = new Date(user.date)
+   let fdate = dateToFormat.toLocaleDateString('hi', options)
+
+    input.innerText = fdate;
+
+    nameContainer.appendChild(div);
+    div.appendChild(p);
+    div.appendChild(input);
+  });
+};
+
+addUser = () => {
+  let name = document.getElementById("name");
+  let date = document.getElementById("date");
+  let tempUser = {
+    name: name.value,
+    date: date.value,
+  };
+  users.push(tempUser);
+  //   console.log(users);
+
+  renderNames();
+  name.value = "";
+};
+
+btn.addEventListener("click", addUser);
